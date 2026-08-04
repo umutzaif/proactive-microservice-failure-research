e788f5bc2db54bfd1162495d88fd7174f4b983dc
Umut Zaif <umutzaif124@gmail.com>
2026-08-04 15:49:03 +0300

    fix: stop treating minikube profile as kubeconfig

--- p0-env/scripts/env.ps1
+++ p0-env/scripts/env.ps1
@@ -1,4 +1,2 @@
 $env:Path += ';C:\Program Files\Docker\Docker\resources\bin;C:\ProgramData\chocolatey\bin'
 $env:MINIKUBE_HOME = Join-Path $PSScriptRoot '..\state\minikube'
-$env:KUBECONFIG = Join-Path $env:MINIKUBE_HOME '.minikube\profiles\p0-online-boutique\config'
-
